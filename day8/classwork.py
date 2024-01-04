d66b1a49f78140aa522568fe59c9955eb923f359
Giorg1B1b1lashv1l1 <giorgibibilashvili97@gmail.com>
2024-01-04 16:12:35 +0400

    8th commit

---
+++ day8/classwork.py
@@ -0,0 +1,61 @@
+# m = [
+#     [1, 2, 3],
+#     [4, 5, 6]    #Matrix
+#     ]
+# print(m[1][2])
+
+
+
+
+# my_str = "Hello world!"
+# print(my_str[6])
+
+
+
+# nums = [5, 8, 2]
+# nums[1] = 42
+
+# print(nums)
+
+
+
+# nums = [1, 2, 3]
+# print(nums + [4, 5, 6])
+
+
+#For loop______
+# i=0
+# while i<10:
+#     print(i)
+#     i+=1#ინკრემენტაცია
+
+
+# for i in range(1,20,3):
+#     print(i)
+
+my_list=["nika","luka","giorgi","vako"]
+for element in my_list:
+    print(element)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
