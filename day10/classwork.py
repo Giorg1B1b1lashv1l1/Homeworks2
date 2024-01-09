f87a398e479c4b9ae238a2ac7edd96efa2077a10
Giorg1B1b1lashv1l1 <giorgibibilashvili97@gmail.com>
2024-01-09 20:59:49 +0400

    10th commit

---
+++ day10/classwork.py
@@ -0,0 +1,14 @@
+#string  function
+# nums = [4, 5, 6]
+# msg = "Numbers: {0} {1} {2}". format(nums[0], nums[1], nums[2])
+# print(msg)
+
+
+
+
+# family_memebers=["ledy","guja","luka"]
+# fml="my mom is {} , my dad is {}, my brother is {}".format(family_memebers[0],family_memebers[1],family_memebers[2])
+# print(fml)
+
+my_name="my name is {} my surname is {} and my age is {}".format("giorgi","bibilashvili","20")
+print(my_name)
